067964c9d7f8c23e1c252d75191c66c41361423c
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-10-15 12:51:06 +0300

    First Commit With Test

---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+function SignUp() {
+  return (
+    <div className="sign-up">
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input id="naem" type="text" placeholder="Your Name ..." />
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
