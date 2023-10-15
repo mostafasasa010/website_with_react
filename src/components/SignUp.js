a53cbcc7d11ef4214d25241b2637cf4ab1a811fe
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-10-15 16:52:40 +0300

    Created Sign Up Style

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
+
 function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordR, setPasswordR] = useState("");
+  function submit(e) {
+    e.preventDefault();
+  }
   return (
     <div className="sign-up">
-      <form>
+      <form onSubmit={submit}>
         <label htmlFor="name">Name: </label>
-        <input id="naem" type="text" placeholder="Your Name ..." />
+        <input
+          id="name"
+          type="text"
+          placeholder="Your Name ..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="email">Email: </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Your Email ..."
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Your Password ..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="repeat">Repeat Password: </label>
+        <input
+          id="repeat"
+          type="password"
+          placeholder="Repeat Password ..."
+          value={passwordR}
+          onChange={(e) => setPasswordR(e.target.value)}
+        />
+        <label></label>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
